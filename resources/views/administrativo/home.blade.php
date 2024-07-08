a01a2ac662f2b1b39dce730a6dcd056a6222a3d0
igarfs <0001020499@senaimgaluno.com.br>
2024-07-08 16:27:16 -0300

    protecao da chave do ggogle-maps

--- resources/views/administrativo/home.blade.php
+++ resources/views/administrativo/home.blade.php
@@ -14,10 +14,8 @@
      and consistent behavior across browsers, consider loading using Promises
      with https://www.npmjs.com/package/@googlemaps/js-api-loader.
     -->
-    <script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBi9y5a2nDWRix0IEa3RqyVHz8ANK1eD8U&callback=initMap&v=weekly&solution_channel=GMP_CCS_simpleclickevents_v2"
-      defer
-    ></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&callback=initMap" async defer></script>
+
 </div>
 
 <div id="elements">

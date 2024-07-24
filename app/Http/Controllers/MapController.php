6a0cdd789be4bf9ccf16729c57e44415fafa8312
igarfs <0001020499@senaimgaluno.com.br>
2024-07-24 01:03:21 -0300

    ganhamo poha

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -11,8 +10,70 @@ class MapController extends Controller
 {
     public function administrativohome()
     {
-        // Busca todas as localizações
-        $locations = dados_anp::all(['id as id', 'LATITUDE_BASE_DD as lat', 'LONGITUDE_BASE_DD as lng', 'CAMPO as title'])->toArray();
+        // Busca todas as localizações com todos os campos explicitamente nomeados
+        $locations = dados_anp::all([
+            'id',
+            'POCO',
+            'CADASTRO',
+            'OPERADOR',
+            'POCO_OPERADOR',
+            'ESTADO',
+            'BACIA',
+            'BLOCO',
+            'SIG_CAMPO',
+            'CAMPO',
+            'TERRA_MAR',
+            'POCO_POS_ANP',
+            'TIPO',
+            'CATEGORIA',
+            'RECLASSIFICACAO',
+            'SITUACAO',
+            'INICIO',
+            'TERMINO',
+            'CONCLUSAO',
+            'TITULARIDADE',
+            'LATITUDE_BASE_4C',
+            'LONGITUDE_BASE_4C',
+            'LATITUDE_BASE_DD',
+            'LONGITUDE_BASE_DD',
+            'DATUM_HORIZONTAL',
+            'TIPO_DE_COORDENADA_DE_BASE',
+            'DIRECAO',
+            'PROFUNDIDADE_VERTICAL_M',
+            'PROFUNDIDADE_SONDADOR_M',
+            'PROFUNDIDADE_MEDIDA_M',
+            'REFERENCIA_DE_PROFUNDIDADE',
+            'MESA_ROTATIVA',
+            'COTA_ALTIMETRICA_M',
+            'LAMINA_D_AGUA_M',
+            'DATUM_VERTICAL',
+            'UNIDADE_ESTRATIGRAFICA',
+            'GEOLOGIA_GRUPO_FINAL',
+            'GEOLOGIA_FORMACAO_FINAL',
+            'GEOLOGIA_MEMBRO_FINAL',
+            'CDPE',
+            'AGP',
+            'PC',
+            'PAG',
+            'PERFIS_CONVENCIONAIS',
+            'DURANTE_PERFURACAO',
+            'PERFIS_DIGITAIS',
+            'PERFIS_PROCESSADOS',
+            'PERFIS_ESPECIAIS',
+            'AMOSTRA_LATERAL',
+            'SISMICA',
+            'TABELA_TEMPO_PROFUNDIDADE',
+            'DADOS_DIRECIONAIS',
+            'TESTE_A_CABO',
+            'TESTE_DE_FORMACAO',
+            'CANHONEIO',
+            'TESTEMUNHO',
+            'GEOQUIMICA',
+            'SIG_SONDA',
+            'NOM_SONDA',
+            'ATINGIU_PRESAL',
+            'DHA_ATUALIZACAO'
+        ])->toArray();
 
         return view('administrativo.home', compact('locations'));
     }
--- resources/views/administrativo/home.blade.php
+++ resources/views/administrativo/home.blade.php
@@ -2,68 +2,13 @@
 
 @section('conteudo')
     <div class="backgroundmap">
-        <div id="map" style="height: 500px; width: 100%;"></div>
+        <div id="map"></div>
     </div>
 
     <div id="elements4">
         <div class="bottomdrag">
-            <h1>Detalhes do Poço</h1>
-    <p><strong>Tipo de Poço:</strong> {{ $item->tp }}</p>
-    <p><strong>Cadastro:</strong> {{ $item->cad }}</p>
-    <p><strong>Operador:</strong> {{ $item->ope }}</p>
-    <p><strong>Poço Operador:</strong> {{ $item->pocoOpe }}</p>
-    <p><strong>Estado:</strong> {{ $item->est }}</p>
-    <p><strong>Bacia:</strong> {{ $item->bac }}</p>
-    <p><strong>Bloco:</strong> {{ $item->blo }}</p>
-    <p><strong>Sig Campo:</strong> {{ $item->sigCam }}</p>
-    <p><strong>Campo:</strong> {{ $item->cam }}</p>
-    <p><strong>TERRA MAR:</strong> {{ $item->terrMar }}</p>
-    <p><strong>Poço Pos ANP:</strong> {{ $item->pocoPosAnp }}</p>
-    <p><strong>Tipo:</strong> {{ $item->tipo }}</p>
-    <p><strong>Categoria:</strong> {{ $item->cat }}</p>
-    <p><strong>Reclassificação:</strong> {{ $item->rec }}</p>
-    <p><strong>Situação:</strong> {{ $item->sit }}</p>
-    <p><strong>Início:</strong> {{ $item->ini }}</p>
-    <p><strong>Término:</strong> {{ $item->term }}</p>
-    <p><strong>Conclusão:</strong> {{ $item->con }}</p>
-    <p><strong>Titularidade:</strong> {{ $item->tit }}</p>
-    <p><strong>Datum Horizontal:</strong> {{ $item->datumHor }}</p>
-    <p><strong>Tipo de Coordenada de Base:</strong> {{ $item->tipoCoordBase }}</p>
-    <p><strong>Direção:</strong> {{ $item->dir }}</p>
-    <p><strong>Profundidade Vertical (m):</strong> {{ $item->profVert }}</p>
-    <p><strong>Profundidade Sondador (m):</strong> {{ $item->profSondador }}</p>
-    <p><strong>Profundidade Medida (m):</strong> {{ $item->profMedida }}</p>
-    <p><strong>Referência de Profundidade:</strong> {{ $item->refProf }}</p>
-    <p><strong>Mesa Rotativa:</strong> {{ $item->mesaRot }}</p>
-    <p><strong>Cota Altimétrica (m):</strong> {{ $item->cotaAlt }}</p>
-    <p><strong>Lâmina d'Água (m):</strong> {{ $item->laminaAgua }}</p>
-    <p><strong>Datum Vertical:</strong> {{ $item->datumVert }}</p>
-    <p><strong>Unidade Estratigráfica:</strong> {{ $item->unidadeEstrat }}</p>
-    <p><strong>Geologia Grupo Final:</strong> {{ $item->geolGrupo }}</p>
-    <p><strong>Geologia Formação Final:</strong> {{ $item->geolFormacao }}</p>
-    <p><strong>Geologia Membro Final:</strong> {{ $item->geolMembro }}</p>
-    <p><strong>CDPE:</strong> {{ $item->cdpe }}</p>
-    <p><strong>AGP:</strong> {{ $item->agp }}</p>
-    <p><strong>PC:</strong> {{ $item->pc }}</p>
-    <p><strong>PAG:</strong> {{ $item->pag }}</p>
-    <p><strong>Perfis Convencionais:</strong> {{ $item->perfConv }}</p>
-    <p><strong>Durante Perfuração:</strong> {{ $item->durantePerf }}</p>
-    <p><strong>Perfis Digitais:</strong> {{ $item->perfDig }}</p>
-    <p><strong>Perfis Processados:</strong> {{ $item->perfProc }}</p>
-    <p><strong>Perfis Especiais:</strong> {{ $item->perfEsp }}</p>
-    <p><strong>Amostra Lateral:</strong> {{ $item->amostraLat }}</p>
-    <p><strong>Sísmica:</strong> {{ $item->sismica }}</p>
-    <p><strong>Tabela Tempo Profundidade:</strong> {{ $item->tabelaTempProf }}</p>
-    <p><strong>Dados Direcionais:</strong> {{ $item->dadosDir }}</p>
-    <p><strong>Teste a Cabo:</strong> {{ $item->testeCabo }}</p>
-    <p><strong>Teste de Formação:</strong> {{ $item->testeFormacao }}</p>
-    <p><strong>Canhoneio:</strong> {{ $item->canhoneio }}</p>
-    <p><strong>Testemunho:</strong> {{ $item->testemunho }}</p>
-    <p><strong>Geoquímica:</strong> {{ $item->geoquimica }}</p>
-    <p><strong>Sig Sonda:</strong> {{ $item->sigSonda }}</p>
-    <p><strong>Nome Sonda:</strong> {{ $item->nomSonda }}</p>
-    <p><strong>Atingiu Presal:</strong> {{ $item->atingiPresal }}</p>
-    <p><strong>DHA Atualização:</strong> {{ $item->dhaAtual }}</p>
+            <div class="conteudo"></div>
+
             <div id="dragbutton"></div>
         </div>
     </div>
@@ -83,39 +28,108 @@
             });
 
             var locations = @json($locations);
+            console.log('Locations:', locations);  // Verifique a estrutura dos dados
 
-            // Criar um array de markers
             var markers = locations.map(function(location) {
                 var position = {
-                    lat: parseFloat(location.lat),
-                    lng: parseFloat(location.lng)
+                    lat: parseFloat(location.LATITUDE_BASE_DD),
+                    lng: parseFloat(location.LONGITUDE_BASE_DD)
                 };
 
                 var marker = new google.maps.Marker({
                     position: position,
-                    map: map
+                    map: map,
+                    id: location.id  // Armazenar o ID no marcador
                 });
 
-                marker.addListener("click", () => {
-                    currentMarker = location; // Salva a localização atual
-                    loadPoço(location.id); // Carrega os detalhes do poço
+                marker.addListener("click", function() {
+                    var markerId = this.id;  // Acessar o ID armazenado
 
-                    $("#elements4").toggle();
-                    $(".bottomdrag").css("top", "900px");
+                    // Encontre o local correspondente para obter outras informações
+                    var locationData = locations.find(loc => loc.id == markerId);
+
+                    if (locationData) {
+                        var content = `
+                            <h3>Informações do Marcador</h3>
+                            <p>ID: ${locationData.id}</p>
+                            <p>POCO: ${locationData.POCO}</p>
+                            <p>CADASTRO: ${locationData.CADASTRO}</p>
+                            <p>OPERADOR: ${locationData.OPERADOR}</p>
+                            <p>POCO_OPERADOR: ${locationData.POCO_OPERADOR}</p>
+                            <p>ESTADO: ${locationData.ESTADO}</p>
+                            <p>BACIA: ${locationData.BACIA}</p>
+                            <p>BLOCO: ${locationData.BLOCO}</p>
+                            <p>SIG_CAMPO: ${locationData.SIG_CAMPO}</p>
+                            <p>CAMPO: ${locationData.CAMPO || 'Não disponível'}</p>
+                            <p>TERRA_MAR: ${locationData.TERRA_MAR}</p>
+                            <p>POCO_POS_ANP: ${locationData.POCO_POS_ANP}</p>
+                            <p>TIPO: ${locationData.TIPO}</p>
+                            <p>CATEGORIA: ${locationData.CATEGORIA}</p>
+                            <p>RECLASSIFICACAO: ${locationData.RECLASSIFICACAO}</p>
+                            <p>SITUACAO: ${locationData.SITUACAO}</p>
+                            <p>INICIO: ${locationData.INICIO}</p>
+                            <p>TERMINO: ${locationData.TERMINO}</p>
+                            <p>CONCLUSAO: ${locationData.CONCLUSAO}</p>
+                            <p>TITULARIDADE: ${locationData.TITULARIDADE}</p>
+                            <p>LATITUDE_BASE_4C: ${locationData.LATITUDE_BASE_4C}</p>
+                            <p>LONGITUDE_BASE_4C: ${locationData.LONGITUDE_BASE_4C}</p>
+                            <p>LATITUDE_BASE_DD: ${locationData.LATITUDE_BASE_DD}</p>
+                            <p>LONGITUDE_BASE_DD: ${locationData.LONGITUDE_BASE_DD}</p>
+                            <p>DATUM_HORIZONTAL: ${locationData.DATUM_HORIZONTAL}</p>
+                            <p>TIPO_DE_COORDENADA_DE_BASE: ${locationData.TIPO_DE_COORDENADA_DE_BASE}</p>
+                            <p>DIRECAO: ${locationData.DIRECAO}</p>
+                            <p>PROFUNDIDADE_VERTICAL_M: ${locationData.PROFUNDIDADE_VERTICAL_M}</p>
+                            <p>PROFUNDIDADE_SONDADOR_M: ${locationData.PROFUNDIDADE_SONDADOR_M}</p>
+                            <p>PROFUNDIDADE_MEDIDA_M: ${locationData.PROFUNDIDADE_MEDIDA_M}</p>
+                            <p>REFERENCIA_DE_PROFUNDIDADE: ${locationData.REFERENCIA_DE_PROFUNDIDADE}</p>
+                            <p>MESA_ROTATIVA: ${locationData.MESA_ROTATIVA}</p>
+                            <p>COTA_ALTIMETRICA_M: ${locationData.COTA_ALTIMETRICA_M}</p>
+                            <p>LAMINA_D_AGUA_M: ${locationData.LAMINA_D_AGUA_M}</p>
+                            <p>DATUM_VERTICAL: ${locationData.DATUM_VERTICAL}</p>
+                            <p>UNIDADE_ESTRATIGRAFICA: ${locationData.UNIDADE_ESTRATIGRAFICA}</p>
+                            <p>GEOLOGIA_GRUPO_FINAL: ${locationData.GEOLOGIA_GRUPO_FINAL}</p>
+                            <p>GEOLOGIA_FORMACAO_FINAL: ${locationData.GEOLOGIA_FORMACAO_FINAL}</p>
+                            <p>GEOLOGIA_MEMBRO_FINAL: ${locationData.GEOLOGIA_MEMBRO_FINAL}</p>
+                            <p>CDPE: ${locationData.CDPE}</p>
+                            <p>AGP: ${locationData.AGP}</p>
+                            <p>PC: ${locationData.PC}</p>
+                            <p>PAG: ${locationData.PAG}</p>
+                            <p>PERFIS_CONVENCIONAIS: ${locationData.PERFIS_CONVENCIONAIS}</p>
+                            <p>DURANTE_PERFURACAO: ${locationData.DURANTE_PERFURACAO}</p>
+                            <p>PERFIS_DIGITAIS: ${locationData.PERFIS_DIGITAIS}</p>
+                            <p>PERFIS_PROCESSADOS: ${locationData.PERFIS_PROCESSADOS}</p>
+                            <p>PERFIS_ESPECIAIS: ${locationData.PERFIS_ESPECIAIS}</p>
+                            <p>AMOSTRA_LATERAL: ${locationData.AMOSTRA_LATERAL}</p>
+                            <p>SISMICA: ${locationData.SISMICA}</p>
+                            <p>TABELA_TEMPO_PROFUNDIDADE: ${locationData.TABELA_TEMPO_PROFUNDIDADE}</p>
+                            <p>DADOS_DIRECIONAIS: ${locationData.DADOS_DIRECIONAIS}</p>
+                            <p>TESTE_A_CABO: ${locationData.TESTE_A_CABO}</p>
+                            <p>TESTE_DE_FORMACAO: ${locationData.TESTE_DE_FORMACAO}</p>
+                            <p>CANHONEIO: ${locationData.CANHONEIO}</p>
+                            <p>TESTEMUNHO: ${locationData.TESTEMUNHO}</p>
+                            <p>GEOQUIMICA: ${locationData.GEOQUIMICA}</p>
+                            <p>SIG_SONDA: ${locationData.SIG_SONDA}</p>
+                            <p>NOM_SONDA: ${locationData.NOM_SONDA}</p>
+                            <p>ATINGIU_PRESAL: ${locationData.ATINGIU_PRESAL}</p>
+                            <p>DHA_ATUALIZACAO: ${locationData.DHA_ATUALIZACAO}</p>
+                        `;
+                        $("#elements4").toggle();
+                        $(".conteudo").html(content).show();
+                        $(".bottomdrag").css("top", "900px");
+
+                        // Estilizar dinamicamente a div
+                        styleContentDiv(locationData);
+                    }
                 });
 
                 return marker;
             });
 
-            // Criar o MarkerClusterer com todos os markers
             new markerClusterer.MarkerClusterer({
                 map,
                 markers,
                 renderer: {
-                    render({
-                        count,
-                        position
-                    }) {
+                    render({ count, position }) {
                         return new google.maps.Marker({
                             position,
                             label: String(count),
@@ -129,22 +143,6 @@
             });
         }
 
-        async function loadPoço() {
-    try {
-        const response = await fetch('http://127.0.0.1:8000/conteudo/1');
-
-        if (!response.ok) {
-            throw new Error(`Erro na requisição: ${response.statusText}`);
-        }
-
-        const item = await response.json();
-        var itemElement = document.createElement('p');
-        itemElement.textContent =
-            `Tipo de Poço: ${item.tp}, Cadastro: ${item.cad}, Operador: ${item.ope}, Poço Operador: ${item.pocoOpe}, Estado: ${item.est}, Bacia: ${item.bac}, Bloco: ${item.blo}, Sig Campo: ${item.sigCam}, Campo: ${item.cam}, Terra/Már: ${item.terrMar}, Poço Pos ANP: ${item.pocoPosAnp}, Tipo: ${item.tipo}, Categoria: ${item.cat}, Reclassificação: ${item.rec}, Situação: ${item.sit}, Início: ${item.ini}, Término: ${item.term}, Conclusão: ${item.con}, Titularidade: ${item.tit}, Datum Horizontal: ${item.datumHor}, Tipo de Coordenada de Base: ${item.tipoCoordBase}, Direção: ${item.dir}, Profundidade Vertical: ${item.profVert} m, Profundidade Sondador: ${item.profSondador} m, Profundidade Medida: ${item.profMedida} m, Referência de Profundidade: ${item.refProf}, Mesa Rotativa: ${item.mesaRot}, Cota Altimétrica: ${item.cotaAlt} m, Lâmina d’Água: ${item.laminaAgua} m, Datum Vertical: ${item.datumVert}, Unidade Estratigráfica: ${item.unidadeEstrat}, Geologia Grupo Final: ${item.geolGrupo}, Geologia Formação Final: ${item.geolFormacao}, Geologia Membro Final: ${item.geolMembro}, CDPE: ${item.cdpe}, AGP: ${item.agp}, PC: ${item.pc}, PAG: ${item.pag}, Perfis Convencionais: ${item.perfConv}, Durante Perfuração: ${item.durantePerf}, Perfis Digitais: ${item.perfDig}, Perfis Processados: ${item.perfProc}, Perfis Especiais: ${item.perfEsp}, Amostra Lateral: ${item.amostraLat}, Sísmica: ${item.sismica}, Tabela Tempo Profundidade: ${item.tabelaTempProf}, Dados Direcionais: ${item.dadosDir}, Teste a Cabo: ${item.testeCabo}, Teste de Formação: ${item.testeFormacao}, Canhoneio: ${item.canhoneio}, Testemunho: ${item.testemunho}, Geoquímica: ${item.geoquimica}, Sig Sonda: ${item.sigSonda}, Nom Sonda: ${item.nomSonda}, Atingiu Presal: ${item.atingiuPresal}, DHA Atualização: ${item.dhaAtual}`;
-        itemList.appendChild(itemElement);
-    } catch (error) {
-        console.error('Erro ao carregar poço:', error);
-    }
-}
+        
     </script>
 @endpush
